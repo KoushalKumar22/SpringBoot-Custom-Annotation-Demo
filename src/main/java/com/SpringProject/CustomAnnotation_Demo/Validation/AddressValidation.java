a38a15a6138ee344f5db591339239a05e706df4b
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-10-26 22:01:37 +0530

    Validation

--- src/main/java/com/SpringProject/CustomAnnotation_Demo/Validation/AddressValidation.java
+++ src/main/java/com/SpringProject/CustomAnnotation_Demo/Validation/AddressValidation.java
@@ -1,4 +1,17 @@
 package com.SpringProject.CustomAnnotation_Demo.Validation;
 
+import com.SpringProject.CustomAnnotation_Demo.Validator.EmployeeValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = {EmployeeValidator.class})
 public @interface AddressValidation {
+    String message() default "Fill the Correct City Name";
+    Class<?>[] group() default {};
+    Class<? extends Payload>[] payload() default {};
 }
